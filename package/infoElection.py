77227e2f457acf2265ef692d52b08382b637bf8a
EC2 Default User <ec2-user@ip-172-31-46-125.ec2.internal>
2021-06-29 03:44:00 +0000

    commit Luciano Paniagua

---
+++ package/infoElection.py
@@ -0,0 +1,54 @@
+import json
+import boto3
+import os
+
+voters_table = os.environ['VOTERS_TABLE']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(voters_table)
+
+def getVoter(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    voter_id = path.split("/")[-1] 
+    response = table.get_item(
+        Key={
+            'pk': voter_id,
+            'sk': 'info'
+        }
+    )
+    item = response['Item']
+    return {
+        'statusCode': 200,
+        'body': json.dumps(item)
+    }
+
+
+def putVoter(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    voter_id = path.split("/")[-1] # ["user", "id"]
+    body = json.loads(event["body"])
+    print(body)
+    print(voter_id)
+    item = {
+        'pk': voter_id,
+        'sk': 'info',
+        'name_voter': body["name_voter"],
+        'city': body["city"],
+        'school': body["school"],
+        'booth': body["booth"],
+        'timeOfArrival': body["timeOfArrival"],
+        'country': body["country"]
+    }
+    print(json.dumps(item))
+    table.put_item(
+      Item=item
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
