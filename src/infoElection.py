02b620ddee4deda42fe52a8d0fd48f77b11a8f29
EC2 Default User <ec2-user@ip-172-31-46-125.ec2.internal>
2021-06-30 13:12:09 +0000

    commit 2 Luciano Paniagua

--- src/infoElection.py
+++ src/infoElection.py
@@ -51,4 +51,49 @@ def putVoter(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+    
+def getAbsentee(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    absentee_id = path.split("/")[-1]
+    response = table.get_item(
+        Key={
+            'pk': absentee_id,
+            'sk': 'info'
+        }
+    )
+    item = response['Item']
+    return {
+        'statusCode': 200,
+        'body': json.dumps(item)
+    }
+    
+def putAbsentee(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    absentee_id = path.split("/")[-1] # ["user", "id"]
+    body = json.loads(event["body"])
+    print(body)
+    print(absentee_id)
+    item = {
+        'pk': absentee_id,
+        'sk': 'info',
+        'absentee' : body["absentee"],
+        'name_voter': body["name_voter"],
+        'city': body["city"],
+        'school': body["school"],
+        'booth': body["booth"],
+        'country': body["country"]
+    }
+    print(json.dumps(item))
+    table.put_item(
+      Item=item
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
 
