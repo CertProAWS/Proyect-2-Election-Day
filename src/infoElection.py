9cb9add3c5f64063c12581e726e06e0d2574b410
EC2 Default User <ec2-user@ip-172-31-47-224.us-west-2.compute.internal>
2021-07-01 21:18:32 +0000

    Merge del Codigo Baldivieso-Vargas 2

--- src/infoElection.py
+++ src/infoElection.py
@@ -2,6 +2,8 @@ import json
 import boto3
 import os
 
+from boto3.dynamodb.conditions import Key, Attr
+
 voters_table = os.environ['VOTERS_TABLE']
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(voters_table)
@@ -96,6 +98,50 @@ def putAbsentee(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+def getInvalidFunction(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    invalid = path.split("/")[-1] # ["user", "id"]
+    response = table.scan(
+        FilterExpression=Attr('invalido').eq(True)
+    )
+
+    item = response['Item']
+    return {
+        'statusCode': 200,
+        'body': json.dumps(item)
+    }
+
+def putInvalidFunction(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    cabina_id = path.split("/")[-1] # ["user", "id"]
     
+    body = json.loads(event["body"])
+    print(body)
+    print(cabina_id)
+    item = {
+        'pk': cabina_id,
+        'invalido': body["invalido"],
+        'city': body["city"],
+        'school': body["school"],
+        'resultadoImagen': body["resultadoImagen"],
+        'resultadoFinal': body["resultadoFinal"],
+        'ausentes': body["ausentes"],
+        'votosEmitidos': body["votosEmitidos"],
+        'votosTotales': body["votosTotales"]
+    }
+    print(json.dumps(item))
+    table.put_item(
+      Item=item
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }    
 
 
